846f3a70dde224dbdfafcf705b394eccac723650
Olli Glorioso <olli.glorioso@avanio.com>
2022-07-29 17:55:05 +0300

    Added new keywords to options

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -23,40 +23,40 @@ export const options = [
     ...navigateToWebsites,
     {
         label: "Open: New Tab",
-        keywords: ["open new tab", "empty", "empty tab", "new empty tab"],
+        keywords: ["open new tab", "empty", "empty tab", "new empty tab", "newtab", "emptytab", "newemptytab"],
         action: newTab.bind(this, urls.Google),
     },
     {
         label: "Update: Extension Dark Mode",
-        keywords: ["update extension dark mode", "dark mode", "dark mode extension", "light mode", "update extension light mode", "lght", "drk", "drka", "darka", "litgh", "ligth"],
+        keywords: ["update extension dark mode", "dark mode", "dark mode extension", "light mode", "update extension light mode", "lght", "drk", "drka", "darka", "litgh", "ligth", "darkmode", "lightmode", "modes"],
         action: () => null,
     },
     {
         label: "Open: New Window",
-        keywords: ["open new window", "empty", "empty window", "new empty window"],
+        keywords: ["open new window", "empty", "empty window", "new empty window", "newwindow", "emptywindow", "newemptywindow", "windows"],
         action: newWindow,
     },
     {
         label: "Info: Number of CPUs",
-        keywords: ["cpu_amount", "get number of cpus", "get number of processors", "processors", "cpu amount", "processor amount"],
+        keywords: ["cpu_amount", "get number of cpus", "get number of processors", "processors", "cpu amount", "processor amount", "numberofcpus", "numberofprocessors", "cpuamount"],
     },
     {
         label: "Info: Display Name",
-        keywords: ["info display name", "screen name", "namm", "display's", "displays'"],
+        keywords: ["info display name", "screen name", "namm", "display's", "displays'", "displayname", "infodisplayname"],
     },
     {
         label: "Close: Current Tab",
-        keywords: ["close current tab", "turn off"],
+        keywords: ["close current tab", "turn off", "currenttab", "thistab", "this tab", "closecurrenttab", "closethistab"],
         action: closeTab,
     },
     {
         label: "Close: All Windows",
-        keywords: ["close all windows", "turn off", "close all windows"],
+        keywords: ["close all windows", "turn off", "close all windows", "allwindows", "allwins", "closeallwindows", "closeallwinds"],
         action: closeAllWindows,
     },
     {
         label: "Update: Reload Page",
-        keywords: ["reload page", "refresh", "refresh page", "re fresh", "re"],
+        keywords: ["reload page", "refresh", "refresh page", "re fresh", "re", "realoadpage", "refreshpage"],
         action: reloadTab,
     },
     // The following are not working, but would be great to make them working.
